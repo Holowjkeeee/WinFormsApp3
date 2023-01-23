697695e44ac29bff7f3c3ad00898430ae9afcdcc
Компуктер <@DESKTOP-4Q19D8A>
2023-01-23 19:17:37 +0800

    Добавьте файлы проекта.

---
+++ WinFormsApp3/Length.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp3
+{
+    public enum MeasureType { cel, far, ran, kelv };
+    public class Tempreture
+    {
+        private double value;
+        private MeasureType type;
+        public Tempreture(double value, string type)
+        { this.value = value;
+            this.type = type;
+        }
+        public string Verbose()
+        {
+            return String.Format("{0}{1}", this.value, this.type); 
+            }
+    }
+}
---
+++ WinFormsApp3Tests3/LengthTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinFormsApp3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp3.Tests
+{
+    [TestClass()]
+    public class LengthTests
+    {
+        [TestMethod()]
+        public void VerboseTest()
+        {
+            var Tempreture = new Tempreture(1, MeasureType.cel);
+            Assert.AreEqual("1 cel", Tempreture.Verbose());
+            Assert.Fail();
+        }
+    }
+}
